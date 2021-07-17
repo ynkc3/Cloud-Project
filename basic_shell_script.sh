8a4ae6bface8e38f48af7d3db98aee27a1a34bb8
ynkc3 <61433951+ynkc3@users.noreply.github.com>
2021-07-16 23:14:34 -0500

    refined

--- basic_shell_script.sh
+++ basic_shell_script.sh
@@ -7,5 +7,5 @@ echo $1
 echo $2
 echo $*
 echo $#
-read -p 'enter your visa status:' visa
-echo -e "you are in $visa visa status"
+read -p 'enter your visa statusque:' visa
+echo -e "you are in $visa visa statusque"
